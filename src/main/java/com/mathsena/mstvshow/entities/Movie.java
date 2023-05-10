b144bfe20772137849322a40b245a7283043eb9b
MathSena <MathSena07@hotmail.com>
2023-05-09 23:36:00 -0300

    Criando seed dos filmes

--- src/main/java/com/mathsena/mstvshow/entities/Movie.java
+++ src/main/java/com/mathsena/mstvshow/entities/Movie.java
@@ -17,8 +17,11 @@ public class Movie {
     @Column(name="movie_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 }
