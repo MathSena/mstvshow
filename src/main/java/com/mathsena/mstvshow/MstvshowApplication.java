bb16c8e393d8d7f6408cedc8662b4b78288bc481
MathSena <MathSena07@hotmail.com>
2023-05-09 23:11:36 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/mstvshow/MstvshowApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.mstvshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstvshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstvshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathsena/mstvshow/MstvshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.mstvshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstvshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
